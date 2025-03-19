a966ebc81e228c90b130e7e12fbb2c0f6636a8df
prakhar0602 <prakhar06092002@gmail.com>
2025-03-19 15:06:12 +0530

    [Prakhar Gupta] Added : Ability to Validate Contact Information in Address Book

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # AddressBookJS_2115500108
 
 # [Prakhar Gupta] Added : Ability to store Contact Information in Address Book 
+# [Prakhar Gupta] Added : Ability to Validate Contact Information in Address Book 
--- app.js
+++ app.js
@@ -1,5 +1,41 @@
+class RegexException{ // Class for Regex Exception Handling
+    static checkDetails(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        const nameRegex = /^[A-Z][a-zA-Z]{2,}$/; // Regex for first and Last Name
+        const locationRegex = /^.{4,}$/; // Regex for City, State and Address
+        const zipRegex = /^[0-9]{6,}$/;  // Regex for ZIP Code
+        const phoneNumberRegex = /^[1-9][0-9]{9,}$/; // Regex for phone Number
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Regex for Email
+       
+        if(!nameRegex.test(firstName)){     // Testing FirstName
+            throw "First Name is Invalid";
+        }
+        if(!nameRegex.test(lastName)){      // Testing LastName
+            throw "Last Name is Invalid";
+        }
+        if(!locationRegex.test(address)){   // Testing Address
+            throw "Address should contain atleast 4 charcaters";
+        }
+        if(!locationRegex.test(city)){      // Testing City
+            throw "City should contain atleast 4 charcaters";
+        }
+        if(!locationRegex.test(state)){     // Testing State
+            throw "State should contain atleast 4 charcaters";
+        }
+        if(!zipRegex.test(zip)){            // Testing ZIP Code
+            throw "Zip Code is Invaid";
+        }
+        if(!phoneNumberRegex.test(phoneNumber)){    // Testing Phone Number
+            throw "Phone Number is Invalid";
+        }
+        if(!emailRegex.test(email)){        //Testing Email
+            throw "Email is Invalid";
+        }
+    }
+}
+
 class Contact{ // Contact Class
     constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){ // Initializing Details
+        RegexException.checkDetails(firstName,lastName,address,city,state,zip,phoneNumber,email); // Handling Regex Exception
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -32,18 +68,24 @@ class addressBook{ // Address Book Class
     }
 }
 
-let book = new addressBook(); // Creating Instance of AddressBook
+try{
 
-let contact1 = new Contact("Prakhar","Gupta","ldiuug hdfl hsdfl hsdhlhgd d","agra","UP",282003,8218748718,"skdjfoise@gmail.com")
-let contact2 =new Contact("Karan","Gupta","ldiuug hdfl hsdflbkldnfknbdf hsdhlhgd d","agra","UP",282001,8217748718,"dtsnskdjfoise@gmail.com")
-let contact3 = new Contact("Rahul","Kumar","ldiuug hdfl hsdfl hsdhlhgdlfiobnlfknbd d","agra","UP",282004,8214748718,"skdtnertnerjfoise@gmail.com")
-let contact4 = new Contact("Raj","Gupta","ldiuug hdfl hsdfl lkdfb;jdfbfhsdhlhgd d","agra","UP",282005,82121748718,"skdjfoisetrnrte@gmail.com")
-
-// Adding Contacts to address book
-book.addContact(contact1) 
-book.addContact(contact2)
-book.addContact(contact3)
-book.addContact(contact4)
-
-// printing contact info
-book.printAllContacts()
+    let book = new addressBook(); // Creating Instance of AddressBook
+    
+    let contact1 = new Contact("Prakhar","Gupta","ldiuug hdfl hsdfl hsdhlhgd d","agra","Uttar Pradesh",282003,8218748718,"skdjfoise@gmail.com")
+    let contact2 =new Contact("Karan","Gupta","ldiuug hdfl hsdflbkldnfknbdf hsdhlhgd d","agra","Uttar Pradesh",282001,8217748718,"dtsnskdjfoise@gmail.com")
+    let contact3 = new Contact("Rahul","Kumar","ldiuug hdfl hsdfl hsdhlhgdlfiobnlfknbd d","agra","Uttar Pradesh",282004,8214748718,"skdtnertnerjfoise@gmail.com")
+    let contact4 = new Contact("Raj","Gupta","ldiuug hdfl hsdfl lkdfb;jdfbfhsdhlhgd ","agra","Uttar Pradesh",282005,8212222222718,"skdjfoisetrnrte@gmail.com")
+    
+    // Adding Contacts to address book
+    book.addContact(contact1) 
+    book.addContact(contact2)
+    book.addContact(contact3)
+    book.addContact(contact4)
+    
+    // printing contact info
+    book.printAllContacts()
+}
+catch(e){
+    console.log(e);
+}
